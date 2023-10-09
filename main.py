c2427e42a6f561ead0d8cbb16794553bdac5b634
Luciana Paiva <luciana@vutus.com>
2023-10-09 14:16:36 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 10
+
+# o bloco if só executa se a condição for verdadeira
+
+if num1 == num2: 
+    print(f'{num1} é igual a {num2}')
+
+    print('fim do programa')
+
+num3 = 5
+num4 = 10 
+
+if num3 == num4: # Falso
+    print(f'{num3} é igua a {num4}')
+
+    # O bloco elif so executa se o bloco anterior for falso
+
+elif num3 < num4: 
+    print(f'{num3} é menor que {num4}')
+
+
+    
+
+
+     
